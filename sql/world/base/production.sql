e965cf328f422ee064ed2cc4b3f5c06158539ba5
Timothée Moos <t1997.m@gmx.net>
2022-04-03 12:52:09 +0200

    fix sql-patch (Commands)

--- sql/world/base/production.sql
+++ sql/world/base/production.sql
@@ -3,12 +3,12 @@
 -- If no data, just delete this file.
 
 
-DELETE FROM `command` WHERE name IN ('creature_objscale', 'creature_objscale set');
+DELETE FROM `command` WHERE name IN ('npc_scale', 'npc_scale set','npc_scale reset','gob_scale','gob_scale set','gob_scale reset');
 
 INSERT INTO `command` (`name`, `security`, `help`) VALUES 
 ('npc_scale', 3, 'Syntax: .npc_objscale $subcommand\nType .help creature_objscale to see a list of subcommands\nor .help xp $subcommand to see info on the subcommand.'),
 ('npc_scale set', 3, 'Syntax: .npc_objscale set $scale\nSet scale of selected creature.'),
 ('npc_scale reset', 3, 'Syntax: .npc_objscale reset \nReset scale of selected creature.'),
 ('gob_scale', 3, 'Syntax: .gob_objscale $subcommand\nType .help gob_objscale to see a list of subcommands\nor .help xp $subcommand to see info on the subcommand.'),
-('gob_scale set', 3, 'Syntax: .gob_objscale set $identifier $scale\nSet scale of selected GameObject.');
+('gob_scale set', 3, 'Syntax: .gob_objscale set $identifier $scale\nSet scale of selected GameObject.'),
 ('gob_scale reset', 3, 'Syntax: .gob_objscale set $identifier \nReset scale of selected GameObject.');
